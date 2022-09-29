1f86a6718e177b005b8d3de7fd62955ef18abefa
Sezer Ökmen <szrokmn34@gmail.com>
2022-09-29 17:42:40 +0300

    backgroundcolor finished

--- javaScript.js
+++ javaScript.js
@@ -4,3 +4,10 @@ display.innerText = new Date().toLocaleTimeString("en-GB");   // yerel saati alÄ
 setInterval(() => {
     display.innerText = new Date().toLocaleTimeString("en-GB");
 }, 1000);
+
+document.querySelector('body').addEventListener('mousemove', clockRengiDegistir);
+
+function clockRengiDegistir(e) {   
+    document.querySelector('.yazi').textContent = `r: ${e.clientX % 255} g:${e.clientY % 255} b:${(e.clientX + e.clientY) % 255}`;
+    document.querySelector('#clock').style.backgroundColor = `rgb(${e.clientX % 255}, ${e.clientY % 255}, ${(e.clientX + e.clientY) % 255})`
+}
